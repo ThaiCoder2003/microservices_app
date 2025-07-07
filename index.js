70abef8b97a6c17f37376035f670124c4a9fd1b7
Le Phan Thanh Nhan <com2109nhanle@gmail.com>
2025-07-07 09:20:05 +0700

    Revert "a"
    This reverts commit 559e9ab55f7ef80ad173ef9184717d71a6e6a2f1.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const { spawn } = require('child-process');
+
+const services = [
+  { name: 'user-service', path: './user-service' },
+  { name: 'product-service', path: './product-service' },
+  { name: 'bff', path: './bff' }
+];
+
+services.forEach(service => {
+  const proc = spawn('node', [`${service.path}/index.js`], { stdio: 'inherit' });
+  proc.on('close', code => {
+    console.log(`${service.name} exited with code ${code}`);
+  });
+});
